41ca24765e1bfba9046cb8acf8c44239f53cf5ad
neilchetna <neelchetna18@gmail.com>
2021-11-25 14:45:53 +0530

    first commit

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LandingPage.css";
+
+export default function LandingPage() {
+  return (
+    <div className="Page">
+      <div className="header"></div>
+    </div>
+  );
+}
